0171b4fce861cb3031d4b46280617cb5da5267de
varunvaatsalya <a.varungupta23@gmail.com>
2024-05-31 16:37:58 +0530

    reset

--- smryReport/settings.py
+++ smryReport/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-@6kp7dudz%u6ll1n=d$rllfefv#w^n53bem6i%5$r0o33w$_4e'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@6kp7dudz%u6ll1n=d$rllfefv#w^n53bem6i%5$r0o33w$_4e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower()=='true'
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+ALLOWED_HOSTS = []
 
 
 # Application definition
